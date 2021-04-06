3f62d984f99ef3bca7330c942feb1a3aa5d5896f
Yisi Yin <yinyisi@udel.edu>
2021-04-06 17:08:13 -0400

    insert current url when you used chrome plugin

--- Chrome_Plugin/content.js
+++ Chrome_Plugin/content.js
@@ -5,7 +5,33 @@ function GetTime() {
     });
 }
 
+
+
+function getCurrentTabUrl(callback) {  
+    var queryInfo = {
+      active: true, 
+      currentWindow: true
+    };
+  
+    chrome.tabs.query(queryInfo, function(tabs) {
+      var tab = tabs[0]; 
+      var url = tab.url;
+      callback(url);
+    });
+  }
+  
+  function renderURL(statusText) {
+    document.getElementById('urlDisplay').textContent = statusText;
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    getCurrentTabUrl(function(url) {
+      renderURL(url); 
+    });
+  });
+
 $(document).ready(function() {
     GetTime();
     setInterval( GetTime, 1000 );
 });
+
