f17e7d3d5b4f7f5fa10d0dc88d6fccd7f868feb1
Grice123 <57500851+Grice123@users.noreply.github.com>
2021-04-19 11:42:06 -0400

    Add part 1 and part 3 of chrome plugin

--- Chrome_Plugin/server.py
+++ Chrome_Plugin/server.py
@@ -1,26 +1,156 @@
-def do_post(self):
-   request_headers = self.headers
-   content_length = request_headers.getheaders('content-length')
-   length = int(content_length[0]) if content_length else 0
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import make_response
 
-jsonData = self.rfile.read(length)
-record = jsonData
-data = json.loads(record)
+import sqlite3
+import json
+
+app  =  Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def save():
+    result_data = {"status":-1, "msg":"error"}
+    if  not request.get_data():
+        result_data["msg"] = "fail"
+    else:
+        post_data = request.data.decode('utf-8')
+        post_data_json = json.loads(post_data)
+
+        sql_key_arr = []
+        sql_value_arr = []
+        for post_data_json_key in post_data_json.keys():
+            sql_key_arr.append(post_data_json_key)
+            sql_value_arr.append(post_data_json[post_data_json_key])
+
+        sql_key = ",".join(post_data_json)
+        sql_value = "\",\"".join(sql_value_arr)
 
 
-#Connect to database file
-conn = sqlite3.connect("database name goes here.db")
-try:
-    cur = conn.cursor()
-    message = data['message']
-    cur.execute['INSERT INTO Log(log_messafe)Value(?)',[message]]
-    conn.commit()
+        sql_value = "\"" + sql_value + "\""
+        
+
+        # Connect to the database file
+        conn = sqlite3.connect("sample.db")
+        try:
+            cur = conn.cursor()
+        
+
+            sql = 'INSERT INTO DATA('+sql_key+') VALUES ('+sql_value+')'
+            print sql
+            cur.execute(sql)
+
+            
+            conn.commit()
+            result_data["status"] = 0
+            result_data["msg"] = "save data success"            
+        except Exception as e:
+            print('There was some kind of error:\n\n' + str(e))
+            result_data["msg"] = "save data error"
+            return json.dumps(result_data)        
+        finally:
+            conn.close()
+
+    res = make_response(json.dumps(result_data))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Content-Type'] = 'application/json'
+
+    return res    
+  
 
 
-except Exception as e:
-    print('There was some kind of error:\n\n' + str(e))
-finally:
-    conn.close()
+@app.route('/api/v1/attitude', methods=['GET'])
+def attitude():
+    result = {"status":-1, "msg":"error", "data": []}
+    if request.method == 'GET':
+        start_time = request.args.get('start_time', '')
+        end_time = request.args.get('end_time', '')
+        if("" == start_time or "" == end_time):
 
-self.send_response(200)
-self.end_headers()
+            result["msg"] = "param error"
+        else:
+
+            
+            conn = sqlite3.connect("sample.db")
+            try:
+                cur = conn.cursor()
+                sql = 'select attitude, count(id) as data_count from DATA where time>=\"' + start_time+ '\" and time<=\"' + end_time + '\" group by attitude'
+                print sql
+                cursor = cur.execute(sql)
+                for row in cursor:
+                    print row
+                    temp = {}
+                    temp["key"] = row[0]
+                    temp["value"] = row[1]
+                    result["data"].append(temp)
+                result["status"] = 0
+                result["msg"] = "success"                
+            except Exception as e:
+                print('There was some kind of error:\n\n' + str(e))
+                result["msg"] = "search db error"
+            finally:
+                conn.close()
+
+    else:
+
+        result["msg"] = "not support POST"
+
+    res = make_response(json.dumps(result))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Content-Type'] = 'application/json'
+
+    return res
+
+
+
+@app.route('/api/v1/learning', methods=['GET'])
+def learning():
+    result = {"status":-1, "msg":"error", "data": []}
+    if request.method == 'GET':
+        start_time = request.args.get('start_time', '')
+        end_time = request.args.get('end_time', '')
+        if("" == start_time or "" == end_time):
+
+            result["msg"] = "param error"
+        else:
+
+            
+            conn = sqlite3.connect("sample.db")
+            try:
+                cur = conn.cursor()
+                sql = 'select attitude, count(id) as data_count from DATA where time>=\"' + start_time+ '\" and time<=\"' + end_time + '\" group by learning'
+                print sql
+                cursor = cur.execute(sql)
+                for row in cursor:
+                    print row
+                    temp = {}
+                    temp["key"] = row[0]
+                    temp["value"] = row[1]
+                    result["data"].append(temp)
+                
+                result["status"] = 0
+                result["msg"] = "success"
+
+            except Exception as e:
+                print('There was some kind of error:\n\n' + str(e))
+                result["msg"] = "search db error"
+            finally:
+                conn.close()
+
+    else:
+
+        result["msg"] = "not support POST"
+
+    res = make_response(json.dumps(result))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Content-Type'] = 'application/json'
+
+    return res
+
+
+
+
+
+if  __name__  ==  '__main__':
+
+    app.run(host='0.0.0.0', port=3001, debug=True)
