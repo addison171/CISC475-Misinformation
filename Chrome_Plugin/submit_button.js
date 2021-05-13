15338897448611c3fb829bdeeb70ffda6166e50f
Yisi Yin <yinyisi@udel.edu>
2021-05-14 03:10:38 +0800

    updated

--- Chrome_Plugin/submit_button.js
+++ Chrome_Plugin/submit_button.js
@@ -1,16 +1,7 @@
 // --------------------------------------------------------------------
 // showing the top five websites visited today  
 
-
-function onAnchorClick(event) {
-    chrome.tabs.create({
-      selected: true,
-      url: event.srcElement.href
-    });
-    return false;
-  }
-  
-  function buildPopupDom(divName, data) {
+function documentpop(divName, data) {
     var popupDiv = document.getElementById(divName);
   
     var ul = document.createElement('ul');
@@ -20,7 +11,7 @@ function onAnchorClick(event) {
       var a = document.createElement('a');
       a.href = data[i];
       a.appendChild(document.createTextNode(data[i]));
-      a.addEventListener('click', onAnchorClick);
+      a.addEventListener('click', onclickfunction);
   
       var li = document.createElement('li');
       li.appendChild(a);
@@ -28,12 +19,19 @@ function onAnchorClick(event) {
       ul.appendChild(li);
     }
   }
+
+function onclickfunction(event) {
+    chrome.tabs.create({
+      selected: true,
+      url: event.srcElement.href
+    });
+    return false;
+  }
+  
   
   // Search history to find up to five links that a user has typed in,
   
-  function buildTypedUrlList(divName) {
-    // To look for history items visited in the last week,
-    // subtract a week of microseconds from the current time.
+  function buildlist(divName) {
     var microsecondsPerDay = 1000 * 60 * 60 * 24 * 1;
     var onedayago = (new Date).getTime() - microsecondsPerDay;
   
@@ -49,8 +47,7 @@ function onAnchorClick(event) {
         for (var i = 0; i < historyItems.length; ++i) {
           var url = historyItems[i].url;
           var processVisitsWithUrl = function(url) {
-            // We need the url of the visited item to process the visit.
-            // Use a closure to bind the  url into the callback's args.
+            
             return function(visitItems) {
               processVisits(url, visitItems);
             };
@@ -67,8 +64,7 @@ function onAnchorClick(event) {
     
     var urlToCount = {};
   
-    // Callback for chrome.history.getVisits().  Counts the number of
-    // times a user visited a URL by typing the address.
+
     var processVisits = function(url, visitItems) {
       for (var i = 0, ie = visitItems.length; i < ie; ++i) {
         // Ignore items unless the user typed the URL.
@@ -83,9 +79,7 @@ function onAnchorClick(event) {
         urlToCount[url]++;
       }
   
-      // If this is the final outstanding call to processVisits(),
-      // then we have the final results.  Use them to build the list
-      // of URLs to show in the popup.
+    
       if (!--numRequestsOutstanding) {
         onAllVisitsProcessed();
       }
@@ -104,10 +98,10 @@ function onAnchorClick(event) {
         return urlToCount[b] - urlToCount[a];
       });
   
-      buildPopupDom(divName, urlArray.slice(0, 5));
+      documentpop(divName, urlArray.slice(0, 5));
     };
   }
   
   document.addEventListener('DOMContentLoaded', function () {
-    buildTypedUrlList("typedUrl_div");
+    buildlist("typedUrl_div");
   });
