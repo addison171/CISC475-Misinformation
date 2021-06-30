0e18785464ed8cdf660c6475705c1b6afe421bca
lukehalko <halko@udel.edu>
2021-06-30 10:50:27 -0400

    background.js posts domains to firebase, not URLS

--- Chrome_Plugin/background.js
+++ Chrome_Plugin/background.js
@@ -11,6 +11,15 @@ var userid;
 const generateID = () =>
     Date.now().toString(Math.floor(Math.random() * 20) + 17);
 
+function getDomain(url){ 
+    let newURL = url.toString();
+    arr = newURL.split('/');
+    console.log(arr);
+    for (x in arr){
+
+    }
+    return arr[2];
+}
 //TODO: I am copying many of these functions from content.js but these kinds of shared items should be in some kind of util.js file
 Date.prototype.Format = function (fmt) {
     var o = {
@@ -36,7 +45,8 @@ chrome.storage.sync.get('userid', function(items) {
         console.log("UserId: " + userid);
     }
     else {
-        userid = generateID();
+        //userid = generateID();
+        userid='no urls'
         chrome.storage.sync.set({userid: userid}, function() {
             console.log(userid);
         });
@@ -65,14 +75,16 @@ function tabTimer () {
 
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         var url = tabs[0].url;
+        const domain = getDomain(url);
+        console.log(domain);
         if (active_url == "") {
-            active_url = url;
-        } else if (active_url != url && active_url != "") {
-            console.log("previous active url: " + active_url + ", " + timeOnPage + "s");
+            active_url = domain;
+        } else if (active_url != domain && active_url != "") {
+            console.log("previous active domain: " + active_url + ", " + timeOnPage + "s");
             sendData(userid, active_url, Date(), timeOnPage);
             timeOnPage = 0;
-            active_url = url;
-        } else if (active_url == url) {
+            active_url = domain;
+        } else if (active_url == domain) {
             timeOnPage += 1;
         }
     });
